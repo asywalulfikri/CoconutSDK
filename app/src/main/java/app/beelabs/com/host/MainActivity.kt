fedd4d7294fddcf36a753dc8701a67d035965095
asywalulfikri <asywalulfikri@gmail.com>
2024-02-26 13:43:31 +0700

    update

---
+++ app/src/main/java/app/beelabs/com/host/MainActivity.kt
@@ -0,0 +1,12 @@
+package app.beelabs.com.host
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(app.beelabs.com.host.R.layout.activity_main)
+
+    }
+}
